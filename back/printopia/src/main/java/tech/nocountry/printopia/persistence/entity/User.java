36013a8177df500500ff338d4d3a1efad84df26b
Florencia <Flopy666@hotmail.com>
2024-02-19 23:49:06 -0300

    CU-86a2b31gr - prueba

--- back/printopia/src/main/java/tech/nocountry/printopia/persistence/entity/User.java
+++ back/printopia/src/main/java/tech/nocountry/printopia/persistence/entity/User.java
@@ -12,7 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import tech.nocountry.printopia.enums.Role;
+import tech.nocountry.printopia.persistence.enums.Role;
 
 @Getter
 @Setter
--- back/printopia/src/main/java/tech/nocountry/printopia/persistence/enums/ProductType.java
+++ back/printopia/src/main/java/tech/nocountry/printopia/persistence/enums/ProductType.java
@@ -1,4 +1,4 @@
-package tech.nocountry.printopia.enums;
+package tech.nocountry.printopia.persistence.enums;
 
 public enum ProductType {
     DIGITAL,
